2877091584e6c618683fb43340b6c465bdce47d4
Andreas Noack Jensen <andreasnoackjensen@gmail.com>
2013-08-05 15:59:55 +0200

    Fix Spearman's rank correlation to address #2. Remove tiedrank method with dimension argument. Add Kendall's rank correlation. Enable tests.

--- test/01.jl
+++ test/01.jl
@@ -17,11 +17,12 @@ values, lengths = rle(z)
 @test lengths == [2, 2, 1, 1, 3, 1]
 @test inverse_rle(values, lengths) == z
 
-# X = [1 0; 2 1; 3 0; 4 1; 5 10]
-# y = [5, 3, 4, 2, 5]
-# @assert_approx_eq cov_spearman(X, y)[1] cov_spearman(X[:,1],y)
-# @assert_approx_eq cov_spearman(X) cov_spearman(X, X)
-# @assert_approx_eq cov_spearman(X, y) [-0.25, -0.1875]
+X = [1 0; 2 1; 3 0; 4 1; 5 10]
+y = [5, 3, 4, 2, 5]
+@test_approx_eq cor_spearman(X, y)[1] cor_spearman(X[:,1],y)
+@test_approx_eq cor_spearman(X) cor_spearman(X, X)
+@test_approx_eq cor_spearman(X, y) [-0.102597835208515, -0.081110710565381]
+@test_approx_eq cor_kendall(X,y) [-0.105409255338946, -0.117851130197758]
 
 fnecdf = ecdf(randn(10000000))
 @test_approx_eq_eps fnecdf([-1.96, -1.644854, -1.281552, -0.6744898, 0, 0.6744898, 1.281552, 1.644854, 1.96]) [0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975] 1e-3
