e906bcb386a70a13bdd95571ccace23ecfdfd0d1
Douglas Bates <dmbates@gmail.com>
2014-02-20 10:35:49 -0600

    Updated statquiz.jl to use symbols for DataFrame columns
    This test is not run by StatsBase/runtests.jl because it requires DataFrames and GLM

--- test/statquiz.jl
+++ test/statquiz.jl
@@ -20,7 +20,7 @@ nasty = DataFrame(	label = ["One", "Two", "Three", "Four", "Five", "Six", "Seven
 println(nasty)
 println("\nII Real Numbers:\nII A")
 print("Test rounding: ")
-@test [@sprintf("%1.0f", x) for x in nasty["round"]] == ["1","2","3","4","5","6","7","8","9"]
+@test [@sprintf("%1.0f", x) for x in nasty[:round]] == ["1","2","3","4","5","6","7","8","9"]
 println("OK")
 print("Test math: ")
 @test int(2.6*7 - 0.2) == 18
@@ -30,20 +30,21 @@ println("OK")
 
 print("Test means: ")
 for vars in names(nasty)[2:end]
-	if vars == "miss"
-		@test isna(mean(nasty[vars]))
-	else
-		@test_approx_eq mean(nasty[vars]) nasty[vars][5]
-	end
+    if vars == :miss
+	@test isna(mean(nasty[vars]))
+    else
+	@test_approx_eq mean(nasty[vars]) nasty[vars][5]
+    end
 end
 println("OK")
 
 print("Test standard deviation: ")
 for vars in names(nasty)[[2,5:9]]
-	@test (@sprintf("%.9e", std(nasty[vars])))[1:10] == "2.73861278"
+#	@test (@sprintf("%.9e", std(nasty[vars])))[1:10] == "2.73861278"
+    @test repr(std(nasty[vars]))[1:10] == "2.73861278"
 end
 println("OK")
-# Note: Origin had one more digit but little fails then. R gives the same as Julia. Stata and SAS less precise returning 2.738612714381 and 2.738612780003404")
+# Note: Original had one more digit but little fails then. R gives the same as Julia. Stata and SAS less precise returning 2.738612714381 and 2.738612780003404")
 
 println("\nII D")
 print("Test correlation: ")
@@ -66,19 +67,25 @@ println("OK")
 
 println("\nII F")
 print("Testing regression: ")
-@test_approx_eq coef(lm(big~x, nasty)) [99999990, 1]
+ctable = coeftable(lm(big ~ x, nasty))
+@test typeof(ctable) == CoefTable
+@test_approx_eq ctable.mat[:,1] [99999990, 1]
+
 println("OK")
 
 println("\nIV Regression:\nIV A")
-nasty["x1"] = nasty["x"]
-nasty["x2"] = nasty["x"].^2
-nasty["x3"] = nasty["x"].^3
-nasty["x4"] = nasty["x"].^4
-nasty["x5"] = nasty["x"].^5
-nasty["x6"] = nasty["x"].^6
-nasty["x7"] = nasty["x"].^7
-nasty["x8"] = nasty["x"].^8
-nasty["x9"] = nasty["x"].^9
+nasty[:x1] = nasty[:x]
+nasty[:x2] = nasty[:x].^2
+nasty[:x3] = nasty[:x].^3
+nasty[:x4] = nasty[:x].^4
+nasty[:x5] = nasty[:x].^5
+nasty[:x6] = nasty[:x].^6
+nasty[:x7] = nasty[:x].^7
+nasty[:x8] = nasty[:x].^8
+nasty[:x9] = nasty[:x].^9
+
+## Is it intended that the least squares problem be overdetermined in the lm fit?
+## n = 9 and p = 10 because of the implicit intercept.
 lm(x1~x2+x3+x4+x5+x6+x7+x8+x9, nasty)
 @test_approx_eq coef(lm(x~x, nasty)) [0,1]
 println("OK")
