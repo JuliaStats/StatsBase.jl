7202bbd564775f1954cb5bc07dcf3dacca6f463d
Simon Kornblith <simon@simonster.com>
2014-05-25 17:59:17 -0400

    Split weighted sum from weighted mean

--- src/means.jl
+++ src/means.jl
@@ -44,23 +44,29 @@ end
 
 # Weighted means
 
-mean{T<:Number}(v::AbstractArray{T}, w::WeightVec) = dot(v, values(w)) / sum(w)
+sum(v::BitArray, w::WeightVec) = dot(vec(v), values(w))
+sum(v::SparseMatrixCSC, w::WeightVec) = dot(vec(v), values(w))
+sum(v::AbstractArray, w::WeightVec) = dot(vec(v), values(w))
+mean(v::AbstractArray, w::WeightVec) = sum(v, w) / sum(w)
 
 function wmean{T<:Number}(v::AbstractArray{T}, w::AbstractArray)
     Base.depwarn("wmean is deprecated, use mean(v, weights(w)) instead.", :wmean)
     mean(v, weights(w))
 end
 
+# General Cartesian-based weighted sum across dimensions
 import Base.Cartesian: @ngenerate, @nloops, @nref
-@ngenerate N typeof(r) function _wsum!{T<:Number,N,W<:Real}(r::Union(Array, AbstractVector), v::AbstractArray{T,N},
-                                                            values::AbstractVector{W}, dim::Int)
+@ngenerate N typeof(r) function Base.sum!{T,N,S,W<:Real}(r::AbstractArray{T,N}, v::AbstractArray{S,N},
+                                                         w::WeightVec{W}, dim::Int)
+    vals = values(w)
+    1 <= dim <= N || error("dim = $dim not in range (1,$N)")
     for i = 1:N
-        i == dim || size(r, i) == size(v, i) || error(DimensionMismatch(""))
+        (i == dim && size(r, i) == 1 && size(v, i) == length(vals)) || size(r, i) == size(v, i) || error(DimensionMismatch(""))
     end
     fill!(r, 0)
     weight = zero(W)
     @nloops N i v d->(if d == dim
-                           weight = values[i_d]
+                           weight = vals[i_d]
                            j_d = 1
                        else
                            j_d = i_d
@@ -68,41 +74,33 @@ import Base.Cartesian: @ngenerate, @nloops, @nref
     r
 end
 
-function mean!{T<:Number,W<:Real}(r::Union(Array, AbstractVector), v::AbstractMatrix{T}, w::WeightVec{W}, dim::Int)
-    m, n = size(v)
+# Weighted sum via `A_mul_B!`/`At_mul_B!` for first and last
+# dimensions of compatible arrays. `vec` and `reshape` are only
+# guaranteed not to make a copy for Arrays, so only supports Arrays if
+# these calls may be necessary.
+function Base.sum!{W<:Real}(r::Union(Array, AbstractVector), v::Union(Array, AbstractMatrix), w::WeightVec{W}, dim::Int)
     if dim == 1
-        (length(r) == n && length(w) == m) || throw(DimensionMismatch("Dimensions mismatch"))
-        At_mul_B!(vec(r), v, values(w))
-    elseif dim == 2        
-        (length(r) == m && length(w) == n) || throw(DimensionMismatch("Dimensions mismatch"))
-        A_mul_B!(vec(r), v, values(w))
-    else
-        error("Invalid value of dim.")
-    end
-    return scale!(r, inv(sum(w)))
-end
-
-function mean!{S,N,T<:Number,W<:Real}(r::AbstractArray{S,N}, v::AbstractArray{T,N}, w::WeightVec{W}, dim::Int)
-    m, n = size(v)
-    if dim == 1 && isa(v, Array) && isa(r, Array)
         m = size(v, 1)
         n = div(length(v), m)
-        (length(r) == n && length(w) == m) || throw(DimensionMismatch("Dimensions mismatch"))
-        At_mul_B!(vec(r), reshape(v, m, n), values(w))
-    elseif dim == ndims(v) && isa(v, Array) && isa(r, Array)
+        (length(r) == n && length(w) == m) || throw(DimensionMismatch(""))
+        At_mul_B!(vec(r), isa(v, AbstractMatrix) ? v : reshape(v, m, n), values(w))
+    elseif dim == ndims(v)
         n = size(v, ndims(v))
         m = div(length(v), n)
-        (length(r) == m && length(w) == n) || throw(DimensionMismatch("Dimensions mismatch"))
-        A_mul_B!(vec(r), reshape(v, m, n), values(w))
-    elseif 1 <= dim <= ndims(v)
-        n = size(v, dim)
-        (length(r) == div(length(v), n) && length(w) == n) || throw(DimensionMismatch("Dimensions mismatch"))
-        _wsum!(r, v, values(w), dim)
+        (length(r) == m && length(w) == n) || throw(DimensionMismatch(""))
+        A_mul_B!(vec(r), isa(v, AbstractMatrix) ? v : reshape(v, m, n), values(w))
     else
-        error("Invalid value of dim.")
+        invoke(sum!, (AbstractArray, AbstractArray, typeof(w), Int), r, v, w, dim)
     end
-    return scale!(r, inv(sum(w)))
+    r
 end
 
-mean{T<:Number,W<:Real}(v::AbstractArray{T}, w::WeightVec{W}, dim::Int) =
-    mean!(Array(typeof(one(T) / one(W)), Base.reduced_dims(size(v), dim)), v, w, dim)
+Base.mean!(r::AbstractArray, v::AbstractArray, w::WeightVec, dim::Int) =
+    scale!(Base.sum!(r, v, w, dim), inv(sum(w)))
+
+Base.sum{T<:Number,W<:Real}(v::AbstractArray{T}, w::WeightVec{W}, dim::Int) =
+    sum!(Array(typeof(zero(T)*zero(W) + zero(T)*zero(W)), Base.reduced_dims(size(v), dim)), v, w, dim)
+
+Base.mean{T<:Number,W<:Real}(v::AbstractArray{T}, w::WeightVec{W}, dim::Int) =
+    mean!(Array(typeof((zero(T)*zero(W) + zero(T)*zero(W)) / one(W)), Base.reduced_dims(size(v), dim)), v, w, dim)
+
