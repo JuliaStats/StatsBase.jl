0a8fe950ac83e0737c4a46a452cdd5d4be5d4125
Dahua Lin <lindahua@gmail.com>
2014-06-22 06:34:26 -0500

    updated perf/sampling.jl to include testing on the `sample!` function itself

--- perf/sampling.jl
+++ perf/sampling.jl
@@ -2,6 +2,7 @@
 
 # require the BenchmarkLite package
 using BenchmarkLite
+using StatsBase
 
 import StatsBase: direct_sample!, xmultinom_sample!
 import StatsBase: knuths_sample!, fisher_yates_sample!, self_avoid_sample!
@@ -20,6 +21,12 @@ tsample!(s::Direct, a, x) = direct_sample!(a, x)
 type Xmultinom <: WithRep end
 tsample!(s::Xmultinom, a, x) = xmultinom_sample!(a, x)
 
+type Sample_WRep <: WithRep end
+tsample!(s::Sample_WRep, a, x) = sample!(a, x; replace=true, ordered=false)
+
+type Sample_WRep_Ord <: WithRep end
+tsample!(s::Sample_WRep_Ord, a, x) = sample!(a, x; replace=true, ordered=true)
+
 type Knuths <: NoRep end
 tsample!(s::Knuths, a, x) = knuths_sample!(a, x)
 
@@ -34,6 +41,12 @@ tsample!(s::Seq_A, a, x) = seqsample_a!(a, x)
 
 type Seq_C <: NoRep end
 tsample!(s::Seq_C, a, x) = seqsample_c!(a, x)
+
+type Sample_NoRep <: NoRep end
+tsample!(s::Sample_NoRep, a, x) = sample!(a, x; replace=false, ordered=false)
+
+type Sample_NoRep_Ord <: NoRep end
+tsample!(s::Sample_NoRep_Ord, a, x) = sample!(a, x; replace=false, ordered=true)
 
 
 # config is in the form of (n, k)
@@ -57,7 +70,9 @@ const ks = 2 .^ [1:16]
 ## with replacement
 
 const procs1 = Proc[ SampleProc{Direct}(), 
-                     SampleProc{Xmultinom}() ]
+                     SampleProc{Sample_WRep}(),
+                     SampleProc{Xmultinom}(),                     
+                     SampleProc{Sample_WRep_Ord}() ]
 
 const cfgs1 = vec([(n, k) for k in ks, n in ns])
 
@@ -67,16 +82,18 @@ println()
 ## without replacement
 
 const procs2 = Proc[ SampleProc{Knuths}(), 
-					 SampleProc{Fisher_Yates}(),
-					 SampleProc{Self_Avoid}(),
-					 SampleProc{Seq_A}(),
-					 SampleProc{Seq_C}() ]
+                     SampleProc{Fisher_Yates}(),
+                     SampleProc{Self_Avoid}(),
+                     SampleProc{Sample_NoRep}(),
+                     SampleProc{Seq_A}(),
+                     SampleProc{Seq_C}(),
+                     SampleProc{Sample_NoRep_Ord}() ]
 
 const cfgs2 = (Int, Int)[]
 for n in 5 * (2 .^ [0:11]), k in 2 .^ [1:16]
-	if k < n
-		push!(cfgs2, (n, k))
-	end
+    if k < n
+        push!(cfgs2, (n, k))
+    end
 end
 
 rtable2 = run(procs2, cfgs2; duration=0.2)
