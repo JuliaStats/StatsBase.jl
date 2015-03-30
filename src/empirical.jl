45ae7c0f266fb4e73c794abed8d9443bd2e9d2e2
afternone <afternone@163.com>
2015-03-30 13:52:42 +0800

    Add empirical ccdf
    Add empirical complementary cumulative distribution function (tail distribution)

--- src/empirical.jl
+++ src/empirical.jl
@@ -37,3 +37,37 @@ function ecdf{T<:Real}(X::RealVector{T})
 end
 
 
+## Empirical CCDF
+
+function eccdf{T<:Real}(X::RealVector{T})
+    Xs = sort(X)
+    n = length(X)
+
+    ef(x::Real) = (n - searchsortedfirst(Xs, x) + 1) / n
+
+    function ef(v::Vector)
+        ord = sortperm(v)
+        m = length(v)
+        r = Array(T, m)
+        r0 = 0
+
+        i = 1
+        for x in Xs
+            while i <= m && x >= v[ord[i]]
+                r[ord[i]] = n - r0
+                i += 1
+            end
+            r0 += 1
+            if i > m 
+            	break 
+            end
+        end
+        while i <= m
+            r[ord[i]] = 1
+            i += 1
+        end
+        return r / n
+    end
+
+    return ef
+end
