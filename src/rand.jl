1b715539b670ad50232f5da1c4585ad8baa0a1b0
Alex Arslan <ararslan@comcast.net>
2016-07-11 23:32:47 -0700

    Implemented more efficient algorithms

--- src/rand.jl
+++ src/rand.jl
@@ -20,15 +20,57 @@ end
 randi(K::Int) = rand(RandIntSampler(K))
 randi(a::Int, b::Int) = rand(RandIntSampler(a, b))
 
-# Draw a number from a binomial distribution using the coin flip method
-# described in Non-Uniform Random Variate Generation by Luc Devroye,
-# chapter 10, page 524.
-function rand_binom(n::Real, p::Real)
+# Generating Random Variables and Stochastic Processes by Martin Haugh
+function rand_beta(a::Real, b::Real)
+    y = rand()
+    u = rand()
+    while u > y^(a-1) * (1-y)^(b-1) / beta(a,b)
+        y = rand()
+        u = rand()
+    end
+    return y
+end
+
+# Geometric random variate, Devroye page 499
+function rand_geom(p::Real)
     x = 0
-    for i = 1:n
-        if rand() <= p
-            x += 1
-        end
+    while rand() <= p
+        x += 1
     end
     return x
+end
+
+# Draw a number from a binomial distribution using the first waiting
+# time algorithm described in Non-Uniform Random Variate Generation
+# by Luc Devroye, chapter 10, page 525
+function _rand_binom_waittime(n::Real, p::Real)
+    x = -1
+    s = 0
+    while s > n
+        s += rand_geom(p)
+        x += 1
+    end
+    return x
+end
+
+# Recursive binomial generator, Devroye page 537
+function rand_binom(n::Real, p::Real, t::Int=7)
+    x = 0
+    s = 1
+    if n * p < t
+        return x + s * _rand_binom_waittime(n, p)
+    else
+        i = floor((n + 1) * p)
+        y = rand_beta(i, n + 1 - i)
+        x += s * i
+        if y <= p
+            n -= i
+            p = (p - y) / (1 - y)
+        else
+            s = -s
+            n = i - 1
+            p = (y - p) / y
+        end
+        return x + s * rand_binom(n, p, t)
+    end
 end
