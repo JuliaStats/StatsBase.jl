e25f02cd63516242d43904f9738314eb51e382e2
Alex Arslan <ararslan@comcast.net>
2017-07-22 20:14:45 -0700

    Remove unnecessary invalid escape

--- test/statmodels.jl
+++ test/statmodels.jl
@@ -9,7 +9,7 @@ v4 = [StatsBase.PValue(rand()./10000) for i in 1:3]
 m = rand(3,4)
 @test sprint(show, CoefTable(Any[v1, v2, v3, v4],
     ["Estimate", "Comments", "df", "p"],
-    ["x1", "x2", "x3"])) == """\
+    ["x1", "x2", "x3"])) == """
      Estimate Comments  df     p
 x1   0.112582     Good  88 <1e-4
 x2   0.368314    Great -90 <1e-4
@@ -17,7 +17,7 @@ x3   0.344454      Bad -80 <1e-4
 """
 
 @test sprint(show, CoefTable(m, ["Estimate", "Stderr", "df", "p"],
-    ["x1", "x2", "x3"], 4)) == """\
+    ["x1", "x2", "x3"], 4)) == """
      Estimate   Stderr       df      p
 x1   0.819778 0.844007 0.923676 0.1717
 x2   0.669931  0.67919 0.066098 0.4204
