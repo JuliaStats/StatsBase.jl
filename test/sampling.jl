de3610881e4171d9d61d5deea5bb85983773f415
Andreas Noack <andreasnoackjensen@gmail.com>
2017-08-22 13:04:22 -0400

    New rand(UnitRange) method in Base requires adjustment of some test values (#296)

--- test/sampling.jl
+++ test/sampling.jl
@@ -76,15 +76,24 @@ check_sample_wrep(a, (3, 12), 0; ordered=true)
 
 test_rng_use(sample, 1:10, 10)
 
-#### sampling pairs
+@testset "sampling pairs" begin
 
-srand(1);
+    srand(1)
 
-@test samplepair(2)  ===  (1, 2)
-@test samplepair(10) === (10, 6)
+    if VERSION < v"0.7.0-DEV.1325" # new faster rand(MersenneTwister, UnitRange)
+        @test samplepair(2)  ===  (1, 2)
+        @test samplepair(10) === (10, 6)
 
-@test samplepair([3, 4, 2, 6, 8]) === (6, 2)
-@test samplepair([1, 2])          === (1, 2)
+        @test samplepair([3, 4, 2, 6, 8]) === (6, 2)
+        @test samplepair([1, 2])          === (1, 2)
+    else
+        @test samplepair(2)  ===  (1, 2)
+        @test samplepair(10) === (8, 2)
+
+        @test samplepair([3, 4, 2, 6, 8]) === (2, 6)
+        @test samplepair([1, 2])          === (1, 2)
+    end
+end
 
 test_rng_use(samplepair, 1000)
 
