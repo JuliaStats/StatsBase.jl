dcd7f3f2311d31c1371a64331427fb4d677ce667
ValdarT <valdar.tammik@gmail.com>
2017-10-13 15:03:12 +0300

    Add tests for random tie breaking in ordinalrank

--- test/ranking.jl
+++ test/ranking.jl
@@ -5,7 +5,12 @@ a = [1.0, 2.0, 2.0, 3.0, 4.0, 4.0, 4.0, 5.0]
 x = [3.0, 1.0, 2.0, 4.0, 4.0, 2.0, 5.0, 4.0]  # x is a permutated version of a
 
 @test ordinalrank(a) == [1, 2, 3, 4, 5, 6, 7, 8]
+srand(123);
+@test ordinalrank(a, rand=true) == [1, 3, 2, 4, 5, 6, 7, 8]
+@test ordinalrank(a, rand=true) == [1, 2, 3, 4, 6, 7, 5, 8]
 @test ordinalrank(x) == [4, 1, 2, 5, 6, 3, 8, 7]
+@test ordinalrank(x, rand=true) == [4, 1, 2, 7, 6, 3, 8, 5]
+@test ordinalrank(x, rand=true) == [4, 1, 2, 5, 7, 3, 8, 6]
 
 @test competerank(a) == [1, 2, 2, 4, 5, 5, 5, 8]
 @test competerank(x) == [4, 1, 2, 5, 5, 2, 8, 5]
