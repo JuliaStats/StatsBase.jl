55b211f7b184a269a76c89413ea136fd4a755743
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-01-23 08:47:14 +0000

    generalize ranking (#337)
    Allow to pass any AbstractArray to the ranking functions (they only need to be sortable, which is true much more generally than Array{<:Real} which is the current restriction). Also, one can now pass a custom "less than" function or reverse the order of sorting.

--- src/ranking.jl
+++ src/ranking.jl
@@ -16,7 +16,7 @@ end
 
 
 # Ordinal ranking ("1234 ranking") -- use the literal order resulted from sort
-function ordinalrank!(rks::RealArray, x::RealArray, p::IntegerArray)
+function ordinalrank!(rks::AbstractArray, x::AbstractArray, p::IntegerArray)
     n = _check_randparams(rks, x, p)
 
     if n > 0
@@ -32,18 +32,21 @@ end
 
 
 """
-    ordinalrank(x)
+    ordinalrank(x; lt = isless, rev::Bool = false)
 
 Return the [ordinal ranking](https://en.wikipedia.org/wiki/Ranking#Ordinal_ranking_.28.221234.22_ranking.29)
-("1234" ranking) of a real-valued array.
+("1234" ranking) of an array. The `lt` keyword allows providing a custom "less
+than" function; use `rev=true` to reverse the sorting order.
 All items in `x` are given distinct, successive ranks based on their
-position in `sort(x)`.
+position in `sort(x; lt = lt, rev = rev)`.
+Missing values are assigned rank `missing`.
 """
-ordinalrank(x::RealArray) = ordinalrank!(Array{Int}(uninitialized, size(x)), x, sortperm(x))
+ordinalrank(x::AbstractArray; lt = isless, rev::Bool = false) =
+    ordinalrank!(Array{Int}(uninitialized, size(x)), x, sortperm(x; lt = lt, rev = rev))
 
 
 # Competition ranking ("1224" ranking) -- resolve tied ranks using min
-function competerank!(rks::RealArray, x::RealArray, p::IntegerArray)
+function competerank!(rks::AbstractArray, x::AbstractArray, p::IntegerArray)
     n = _check_randparams(rks, x, p)
 
     if n > 0
@@ -70,18 +73,21 @@ end
 
 
 """
-    competerank(x)
+    competerank(x; lt = isless, rev::Bool = false)
 
 Return the [standard competition ranking](http://en.wikipedia.org/wiki/Ranking#Standard_competition_ranking_.28.221224.22_ranking.29)
-("1224" ranking) of a real-valued
-array. Items that compare equal are given the same rank, then a gap is left
+("1224" ranking) of an array. The `lt` keyword allows providing a custom "less
+than" function; use `rev=true` to reverse the sorting order.
+Items that compare equal are given the same rank, then a gap is left
 in the rankings the size of the number of tied items - 1.
+Missing values are assigned rank `missing`.
 """
-competerank(x::RealArray) = competerank!(Array{Int}(uninitialized, size(x)), x, sortperm(x))
+competerank(x::AbstractArray; lt = isless, rev::Bool = false) =
+    competerank!(Array{Int}(uninitialized, size(x)), x, sortperm(x; lt = lt, rev = rev))
 
 
 # Dense ranking ("1223" ranking) -- resolve tied ranks using min
-function denserank!(rks::RealArray, x::RealArray, p::IntegerArray)
+function denserank!(rks::AbstractArray, x::AbstractArray, p::IntegerArray)
     n = _check_randparams(rks, x, p)
 
     if n > 0
@@ -111,15 +117,18 @@ end
     denserank(x)
 
 Return the [dense ranking](http://en.wikipedia.org/wiki/Ranking#Dense_ranking_.28.221223.22_ranking.29)
-("1223" ranking) of a real-valued array. Items that
+("1223" ranking) of an array. The `lt` keyword allows providing a custom "less
+than" function; use `rev=true` to reverse the sorting order. Items that
 compare equal receive the same ranking, and the next subsequent rank is
 assigned with no gap.
+Missing values are assigned rank `missing`.
 """
-denserank(x::RealArray) = denserank!(Array{Int}(uninitialized, size(x)), x, sortperm(x))
+denserank(x::AbstractArray; lt = isless, rev::Bool = false) =
+    denserank!(Array{Int}(uninitialized, size(x)), x, sortperm(x; lt = lt, rev = rev))
 
 
 # Tied ranking ("1 2.5 2.5 4" ranking) -- resolve tied ranks using average
-function tiedrank!(rks::RealArray, x::RealArray, p::IntegerArray)
+function tiedrank!(rks::AbstractArray, x::AbstractArray, p::IntegerArray)
     n = _check_randparams(rks, x, p)
 
     if n > 0
@@ -158,8 +167,26 @@ end
 
 Return the [tied ranking](http://en.wikipedia.org/wiki/Ranking#Fractional_ranking_.28.221_2.5_2.5_4.22_ranking.29),
 also called fractional or "1 2.5 2.5 4" ranking,
-of a real-valued array. Items that compare equal receive the mean of the
+of an array. The `lt` keyword allows providing a custom "less
+than" function; use `rev=true` to reverse the sorting order.
+Items that compare equal receive the mean of the
 rankings they would have been assigned under ordinal ranking.
+Missing values are assigned rank `missing`.
 """
-tiedrank(x::RealArray) = tiedrank!(Array{Float64}(uninitialized, size(x)), x, sortperm(x))
+tiedrank(x::AbstractArray; lt = isless, rev::Bool = false) =
+    tiedrank!(Array{Float64}(uninitialized, size(x)), x, sortperm(x; lt = lt, rev = rev))
 
+for (f, f!, S) in zip([:ordinalrank, :competerank, :denserank, :tiedrank],
+                      [:ordinalrank!, :competerank!, :denserank!, :tiedrank!],
+                      [Int, Int, Int, Float64])
+    @eval begin
+        function $f(x::AbstractArray{>: Missing}; lt = isless, rev::Bool = false)
+            inds = find(!ismissing, x)
+            xv = disallowmissing(view(x, inds))
+            sp = sortperm(xv; lt = lt, rev = rev)
+            rks = missings($S, length(x))
+            $(f!)(view(rks, inds), xv, sp)
+            rks
+        end
+    end
+end
