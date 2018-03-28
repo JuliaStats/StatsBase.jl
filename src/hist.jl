3f9c3bae5783c371c1d7efe7da62327342eea441
Douglas Bates <dmbates@gmail.com>
2018-03-28 15:42:39 -0500

    Conditionally use broadcast in midpoints

--- src/hist.jl
+++ src/hist.jl
@@ -533,4 +533,8 @@ Calculate the midpoints (pairwise mean of consecutive elements).
 """
 midpoints(v::AbstractVector) = [middle(v[i - 1], v[i]) for i in 2:length(v)]
 
-midpoints(r::AbstractRange) = broadcast(+, r[1:(end - 1)], step(r) / 2)
+if VERSION < v"0.7.0-DEV.4713"
+    midpoints(r::AbstractRange) = r[1:(end - 1)] + step(r) / 2
+else
+    midpoints(r::AbstractRange) = broadcast(+, r[1:(end - 1)], step(r) / 2)
+end
