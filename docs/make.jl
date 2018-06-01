5a75f756faf25449bcb5189d6b19f4c691877f89
Milan Bouchet-Valat <nalimilan@club.fr>
2018-06-01 09:45:22 +0200

    Deploy docs from Julia 0.7
    The functions which have been moved from Base don't appear on the manual when deploying from 0.6.

--- docs/make.jl
+++ docs/make.jl
@@ -23,7 +23,7 @@ makedocs(
 deploydocs(
     repo = "github.com/JuliaStats/StatsBase.jl.git",
     target = "build",
-    julia  = "0.6",
+    julia  = "0.7",
     deps   = nothing,
     make   = nothing
 )
