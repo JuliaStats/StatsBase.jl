0562768b933d75a79bdda78b639496943d4446b4
Mateusz Baran <mateuszbaran89@gmail.com>
2019-02-08 10:09:33 +0100

    Improved docs for cov(ce::CovarianceEstimator, X::AbstractMatrix)

--- src/cov.jl
+++ src/cov.jl
@@ -184,9 +184,13 @@ Compute the covariance matrix of the matrix `X` along dimension `dims`
 using estimator `ce`. A weighting vector `w` can be specified.
 The keyword argument `mean` can be:
 
-    * `nothing` (default) in which case the mean is estimated and subtracted
-    from the data `X`,
-    * a precalculated mean in which case it is subtracted from the data `X`.
+* `nothing` (default) in which case the mean is estimated and subtracted
+  from the data `X`,
+* a precalculated mean in which case it is subtracted from the data `X`.
+  Assuming `size(X)` is `(N,M)`, `mean` can either be:
+  * when `dims=1`, an `AbstractMatrix` of size `(1,M)`,
+  * when `dims=2`, an `AbstractVector` of length `N` or an `AbstractMatrix`
+    of size `(N,1)`.
 """
 cov(ce::CovarianceEstimator, X::AbstractMatrix; mean=nothing, dims::Int=1) =
     error("cov is not defined for $(typeof(ce)) and $(typeof(X))")
