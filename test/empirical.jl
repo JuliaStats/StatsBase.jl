b2cb36c8c31ec3b182dc18229339ab6a1b56ebcb
John Zito <johnczito@gmail.com>
2019-06-26 23:53:01 -0400

    unit tests for weighted ecdf

--- test/empirical.jl
+++ test/empirical.jl
@@ -13,3 +13,19 @@ using Test
     @test fnecdf([zeros(5000); ones(5000)]) == [zeros(5000); ones(5000)]
     @test extrema(fnecdf) == (minimum(fnecdf), maximum(fnecdf)) == (0.5, 0.5)
 end
+
+@testset "Weighted ECDF" begin
+    x = randn(10000000)
+    w = weights(rand(10000000))
+    fnecdf = ecdf(x, w)
+    y = [-1.96, -1.644854, -1.281552, -0.6744898, 0, 0.6744898, 1.281552, 1.644854, 1.96]
+    @test fnecdf.weights.values != w.values
+    @test fnecdf(y) ≈ map(fnecdf, y)
+    @test extrema(fnecdf) == (minimum(fnecdf), maximum(fnecdf)) == extrema(x)
+    fnecdf = ecdf([1.0, 0.5], weights([3, 1]))
+    @test fnecdf(0.75) == 0.25
+    @test extrema(fnecdf) == (minimum(fnecdf), maximum(fnecdf)) == (0.5, 1.0)
+
+    @test_throws MethodError ecdf(rand(8), rand(10))
+
+end
