0b2e9745ce0a8ecc7ce57ce774e7c8615d7ca764
John Zito <johnczito@gmail.com>
2019-07-05 12:16:16 -0400

    make `weights` empty when they're even

--- src/empirical.jl
+++ src/empirical.jl
@@ -9,24 +9,27 @@ struct ECDF{T <: AbstractVector{<:Real}, W <: AbstractWeights{<:Real}}
 end
 
 function (ecdf::ECDF)(x::Real)
-    weightsum = sum(ecdf.weights)
     n = searchsortedlast(ecdf.sorted_values, x)
-    sum(view(ecdf.weights, 1:n)) / weightsum
+    evenweights = isempty(ecdf.weights)
+    weightsum = evenweights ? length(ecdf.sorted_values) : sum(ecdf.weights)
+    partialsum = evenweights ? n : sum(view(ecdf.weights, 1:n))
+    partialsum / weightsum
 end
 
 function (ecdf::ECDF)(v::RealVector)
+    evenweights = isempty(ecdf.weights)
+    weightsum = evenweights ? length(ecdf.sorted_values) : sum(ecdf.weights)
     ord = sortperm(v)
     m = length(v)
     r = similar(ecdf.sorted_values, m)
-    r0 = 0
+    r0 = zero(weightsum)
     i = 1
-    weightsum = sum(ecdf.weights)
     for (j, x) in enumerate(ecdf.sorted_values)
         while i <= m && x > v[ord[i]]
             r[ord[i]] = r0
             i += 1
         end
-        r0 += ecdf.weights[j]
+        r0 += evenweights ? 1 : ecdf.weights[j]
         if i > m
             break
         end
@@ -50,6 +53,8 @@ evaluate CDF values on other samples.
 `extrema`, `minimum`, and `maximum` are supported to for obtaining the range over which
 function is inside the interval ``(0,1)``; the function is defined for the whole real line.
 """
+ecdf(X::RealVector{T}) where T<:Real = ECDF(sort(X), weights(Float64[]))
+
 function ecdf(X::RealVector{T}, w::AbstractWeights{W} = weights(ones(length(X)))) where {T, W <: Real}
     length(X) == length(w) || throw(ArgumentError("data and weight vectors must be the same size," *
         "got $(length(X)) and $(length(w))"))
