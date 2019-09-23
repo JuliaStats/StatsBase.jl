dea5fc454cf2c8faf77af4f4074ad51dea011c20
Luca Bittarello <lbittarello@users.noreply.github.com>
2019-09-23 20:13:46 +0200

    Update src/deprecates.jl
    Co-Authored-By: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/deprecates.jl
+++ src/deprecates.jl
@@ -38,5 +38,5 @@ end
 
 ### Deprecated September 2019
 @deprecate sum(A::AbstractArray, w::AbstractWeights, dims::Int) sum(A, w, dims=dims)
-@deprecate values(wv::AbstractWeights{S, T, V}) where {S, T, V} convert(V, wv)
+@deprecate values(wv::AbstractWeights) convert(Vector, wv)
 @deprecate values(wv::UnitWeights) convert(Vector, wv)
