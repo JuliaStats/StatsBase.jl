668a92d4d11c25f44731a66d9cbcf6664221aa5a
sdewaele <14310676+sdewaele@users.noreply.github.com>
2020-12-06 11:46:13 -0500

    Update src/transformations.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/transformations.jl
+++ src/transformations.jl
@@ -269,7 +269,7 @@ function fit(::Type{UnitRangeTransform}, X::AbstractMatrix{<:Real};
     dims ∈ [1,2] || throw(DomainError(dims, "fit only accept dims to be 1 or 2."))
     tmin_tmax = extrema(X; dims=dims)
     tmin, tmax = (vec(getindex.(tmin_tmax, i)) for i in 1:2)
-    @inbounds @. tmax = 1 / (tmax - tmin)
+    @. tmax = 1 / (tmax - tmin)
     l = length(tmin)
     return UnitRangeTransform(l, dims, unit, tmin, tmax)
 end
