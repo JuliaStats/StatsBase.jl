c8672a3689027897abd3d209e5d9cdcd506d1f05
Philip Swannell <philip.swannell@gmail.com>
2021-01-27 16:46:11 +0000

    added tests

--- test/rankcorr.jl
+++ test/rankcorr.jl
@@ -26,14 +26,16 @@ c22 = corspearman(x2, x2)
 
 # corkendall
 
-@test_throws ErrorException("Vectors must have same length") corkendall([1,2,3,4],[1,2,3])
+@test_throws ErrorException("Vectors must have same length") corkendall([1,2,3,4], [1,2,3])
+@test isnan(corkendall([1,2], [3,NaN]))
+@test isnan(corkendall([1,1,1], [1,2,3]))
 
 @test corkendall(x1, y) == -1 / sqrt(90)
 @test corkendall(x2, y) == -1 / sqrt(72)
 @test corkendall(X, y) == [-1 / sqrt(90), -1 / sqrt(72)]
 @test corkendall(y, X) == [-1 / sqrt(90) -1 / sqrt(72)]
 
-n = 100_000 # so vectors tested are 5 times this length
+n = 100_000
 @test corkendall(repeat(x1, n), repeat(y, n)) ≈ -1 / sqrt(90)
 @test corkendall(repeat(x2, n), repeat(y, n)) ≈ -1 / sqrt(72)
 @test corkendall(repeat(X, n), repeat(y, n)) ≈ [-1 / sqrt(90), -1 / sqrt(72)]
@@ -45,17 +47,17 @@ c22 = corkendall(x2, x2)
 
 @test c11 == 1.0
 @test c22 == 1.0
-@test c12 == 6 / sqrt(80)
+@test c12 == 3 / sqrt(20)
 
 @test corkendall(X, X) ≈ [c11 c12; c12 c22]
 @test corkendall(X)    ≈ [c11 c12; c12 c22]
 
 @test corkendall(repeat(X, n), repeat(X, n)) ≈ [c11 c12; c12 c22]
-@test corkendall(repeat(X, n))                  ≈ [c11 c12; c12 c22]
+@test corkendall(repeat(X, n))               ≈ [c11 c12; c12 c22]
 
-
-@test corkendall(collect(1:100_000), collect(1:100_000)) == 1.0
-@test corkendall(collect(1:100_000), reverse(collect(1:100_000))) == -1.0
+@test corkendall(collect(1:n), collect(1:n)) == 1.0
+@test corkendall(collect(1:n), reverse(collect(1:n))) == -1.0
+@test isnan(corkendall(repeat([1], n), collect(1:n)))
 
 @test corkendall(repeat([0,1,1,0], n), repeat([1,0,1,0], n)) == 0.0
 
