56cb219ff3c66ebdcc65465dcc53c7b824cd79e8
Philip Swannell <philip.swannell@gmail.com>
2021-01-31 18:44:31 +0000

    changes following review by nalimilan

--- src/rankcorr.jl
+++ src/rankcorr.jl
@@ -1,13 +1,13 @@
 # Rank-based correlations
-#
+# 
 # - Spearman's correlation
 # - Kendall's correlation
-#
+# 
 
 #######################################
-#
+# 
 #   Spearman correlation
-#
+# 
 #######################################
 
 """
@@ -28,9 +28,9 @@ corspearman(X::RealMatrix) = (Z = mapslices(tiedrank, X, dims=1); cor(Z, Z))
 
 
 #######################################
-# 
+#
 #   Kendall correlation
-# 
+#
 #######################################
 
 # Knight, William R. “A Computer Method for Calculating Kendall's Tau with Ungrouped Data.”
@@ -47,7 +47,7 @@ function corkendall!(x::RealVector, y::RealVector, permx::AbstractVector{<:Integ
 
     npairs = float(n) * (n - 1) / 2
     # ntiesx, ntiesy, ndoubleties are floats to avoid overflows on 32bit
-    ntiesx, ntiesy, ndoubleties, k, nswaps = 0.0, 0.0, 0.0, 0, 0
+    ntiesx, ntiesy, ndoubleties, k, nswaps = widen(0), widen(0), widen(0), widen(0), widen(0)
 
     @inbounds for i = 2:n
         if x[i - 1] == x[i]
@@ -57,14 +57,14 @@ function corkendall!(x::RealVector, y::RealVector, permx::AbstractVector{<:Integ
             # sorted first on x, then (where x values are tied) on y. Hence 
             # double ties can be counted by calling countties.
             sort!(view(y, (i - k - 1):(i - 1)))
-            ntiesx += float(k) * (k + 1) / 2
+            ntiesx += div(k * (k + 1), 2)
             ndoubleties += countties(y,  i - k - 1, i - 1)
             k = 0
         end
     end
     if k > 0
         sort!(view(y, ((n - k):n)))
-        ntiesx += float(k) * (k + 1) / 2
+        ntiesx += div(k * (k + 1), 2)
         ndoubleties += countties(y,  n - k, n)
     end
 
@@ -72,7 +72,7 @@ function corkendall!(x::RealVector, y::RealVector, permx::AbstractVector{<:Integ
     ntiesy = countties(y, 1, n)
 
     (npairs + ndoubleties - ntiesx - ntiesy - 2 * nswaps) /
-     sqrt((npairs - ntiesx) * (npairs - ntiesy))
+     sqrt(float(npairs - ntiesx) * float(npairs - ntiesy))
 end
 
 """
@@ -81,20 +81,20 @@ end
 Return the number of ties within `x[lo:hi]`. Assumes `x` is sorted. 
 """
 function countties(x::AbstractVector, lo::Integer, hi::Integer)
-    # avoid overflows on 32 bit by using floats
-    thistiecount, result = 0.0, 0.0
+    # avoid overflows on both 32 & 64 bit by using widen
+    thistiecount, result = widen(0), widen(0)
     (lo < 1 || hi > length(x)) && error("Bounds error")
     @inbounds for i = (lo + 1):hi
         if x[i] == x[i - 1]
-            thistiecount += 1.0
+            thistiecount += 1
         elseif thistiecount > 0
-            result += thistiecount * (thistiecount + 1) / 2
-            thistiecount = 0.0
+            result += div(thistiecount * (thistiecount + 1), 2)
+            thistiecount = 0
         end
     end
 
     if thistiecount > 0
-        result += thistiecount * (thistiecount + 1) / 2
+        result += div(thistiecount * (thistiecount + 1), 2)
     end
     result
 end
--- test/rankcorr.jl
+++ test/rankcorr.jl
@@ -30,16 +30,16 @@ c22 = corspearman(x2, x2)
 @test isnan(corkendall([1,2], [3,NaN]))
 @test isnan(corkendall([1,1,1], [1,2,3]))
 
-@test corkendall(x1, y) == -1 / sqrt(90)
-@test corkendall(x2, y) == -1 / sqrt(72)
-@test corkendall(X, y) == [-1 / sqrt(90), -1 / sqrt(72)]
-@test corkendall(y, X) == [-1 / sqrt(90) -1 / sqrt(72)]
+@test corkendall(x1, y) == -1/sqrt(90)
+@test corkendall(x2, y) == -1/sqrt(72)
+@test corkendall(X, y)  == [-1/sqrt(90), -1/sqrt(72)]
+@test corkendall(y, X)  == [-1/sqrt(90) -1/sqrt(72)]
 
 n = 100_000
-@test corkendall(repeat(x1, n), repeat(y, n)) ≈ -1 / sqrt(90)
-@test corkendall(repeat(x2, n), repeat(y, n)) ≈ -1 / sqrt(72)
-@test corkendall(repeat(X, n), repeat(y, n)) ≈ [-1 / sqrt(90), -1 / sqrt(72)]
-@test corkendall(repeat(y, n), repeat(X, n)) ≈ [-1 / sqrt(90) -1 / sqrt(72)]
+@test corkendall(repeat(x1, n), repeat(y, n)) ≈ -1/sqrt(90)
+@test corkendall(repeat(x2, n), repeat(y, n)) ≈ -1/sqrt(72)
+@test corkendall(repeat(X, n), repeat(y, n))  ≈ [-1/sqrt(90), -1/sqrt(72)]
+@test corkendall(repeat(y, n), repeat(X, n))  ≈ [-1/sqrt(90) -1/sqrt(72)]
 
 c11 = corkendall(x1, x1)
 c12 = corkendall(x1, x2)
@@ -47,7 +47,7 @@ c22 = corkendall(x2, x2)
 
 @test c11 == 1.0
 @test c22 == 1.0
-@test c12 == 3 / sqrt(20)
+@test c12 == 3/sqrt(20)
 
 @test corkendall(X, X) ≈ [c11 c12; c12 c22]
 @test corkendall(X)    ≈ [c11 c12; c12 c22]
@@ -55,34 +55,38 @@ c22 = corkendall(x2, x2)
 @test corkendall(repeat(X, n), repeat(X, n)) ≈ [c11 c12; c12 c22]
 @test corkendall(repeat(X, n))               ≈ [c11 c12; c12 c22]
 
-@test corkendall(collect(1:n), collect(1:n)) == 1.0
+@test corkendall(collect(1:n), collect(1:n))          == 1.0
 @test corkendall(collect(1:n), reverse(collect(1:n))) == -1.0
+
 @test isnan(corkendall(repeat([1], n), collect(1:n)))
 
 @test corkendall(repeat([0,1,1,0], n), repeat([1,0,1,0], n)) == 0.0
 
 z = [1  1  1;
-    1  1  2;
-    1  2  2;
-    1  2  2;
-    1  2  1;
-    2  1  2;
-    1  1  2;
-    2  2  2]
+     1  1  2;
+     1  2  2;
+     1  2  2;
+     1  2  1;
+     2  1  2;
+     1  1  2;
+     2  2  2]
 
-@test corkendall(z)   == [1 0 1 / 3; 0 1 0;1 / 3 0 1]
-@test corkendall(z, z)   == [1 0 1 / 3; 0 1 0;1 / 3 0 1]
-@test corkendall(z[:,1], z) == [1 0 1 / 3]
-@test corkendall(z, z[:,1]) == [1;0;1 / 3]
+@test corkendall(z)         == [1 0 1/3; 0 1 0; 1/3 0 1]
+@test corkendall(z, z)      == [1 0 1/3; 0 1 0; 1/3 0 1]
+@test corkendall(z[:,1], z) == [1 0 1/3]
+@test corkendall(z, z[:,1]) == [1; 0; 1/3]
 
 z = float(z)
-@test corkendall(z)   == [1 0 1 / 3; 0 1 0;1 / 3 0 1]
-@test corkendall(z, z)   == [1 0 1 / 3; 0 1 0;1 / 3 0 1]
-@test corkendall(z[:,1], z) == [1 0 1 / 3]
-@test corkendall(z, z[:,1]) == [1;0;1 / 3]
+@test corkendall(z)         == [1 0 1/3; 0 1 0; 1/3 0 1]
+@test corkendall(z, z)      == [1 0 1/3; 0 1 0; 1/3 0 1]
+@test corkendall(z[:,1], z) == [1 0 1/3]
+@test corkendall(z, z[:,1]) == [1; 0; 1/3]
 
 w = repeat(z, n)
-@test corkendall(w)   == [1 0 1 / 3; 0 1 0;1 / 3 0 1]
-@test corkendall(w, w)   == [1 0 1 / 3; 0 1 0;1 / 3 0 1]
-@test corkendall(w[:,1], w) == [1 0 1 / 3]
-@test corkendall(w, w[:,1]) == [1;0;1 / 3]
+@test corkendall(w)         == [1 0 1/3; 0 1 0; 1/3 0 1]
+@test corkendall(w, w)      == [1 0 1/3; 0 1 0; 1/3 0 1]
+@test corkendall(w[:,1], w) == [1 0 1/3]
+@test corkendall(w, w[:,1]) == [1; 0; 1/3]
+
+StatsBase.midpoint(1,10)        == 5
+StatsBase.midpoint(1,widen(10)) == 5
