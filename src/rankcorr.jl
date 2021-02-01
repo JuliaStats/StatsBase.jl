f9114c148de72a1f09e25cea0a97d8aebbda647c
Philip Swannell <philip.swannell@gmail.com>
2021-02-01 12:16:53 +0000

    Update rankcorr.jl
    Incorporated suggestions from nalimilan. Also further changes to avoid overflow errors

--- src/rankcorr.jl
+++ src/rankcorr.jl
@@ -46,8 +46,8 @@ function corkendall!(x::RealVector, y::RealVector, permx::AbstractVector{<:Integ
     permute!(y, permx)
     
     # Use widen to avoid overflows on both 32bit and 64bit
-    npairs = widen(n) * (n - 1) / 2
-    ntiesx, ntiesy, ndoubleties, nswaps = widen(0), widen(0), widen(0), widen(0)
+    npairs = div(widen(n) * (n - 1), 2)
+    ntiesx, ndoubleties, nswaps = widen(0), widen(0), widen(0)
     k = 0
 
     @inbounds for i = 2:n
@@ -57,47 +57,25 @@ function corkendall!(x::RealVector, y::RealVector, permx::AbstractVector{<:Integ
             # Sort the corresponding chunk of y, so the rows of hcat(x,y) are 
             # sorted first on x, then (where x values are tied) on y. Hence 
             # double ties can be counted by calling countties.
-            sort!(view(y, (i - k - 1):(i - 1)))
-            ntiesx += div(k * (k + 1), 2)
+            sort!(view(y, (i - k - 1):(i - 1)))  # Can't use wide integers here
+            ntiesx += div(widen(k) * (k + 1), 2) # Must use wide integers here
             ndoubleties += countties(y,  i - k - 1, i - 1)
             k = 0
         end
     end
     if k > 0
         sort!(view(y, ((n - k):n)))
-        ntiesx += div(k * (k + 1), 2)
+        ntiesx += div(widen(k) * (k + 1), 2)
         ndoubleties += countties(y,  n - k, n)
     end
 
-    nswaps = msort!(y, 1, n)
+    nswaps = merge_sort!(y, 1, n)
     ntiesy = countties(y, 1, n)
 
+    # Calls to float below prevent possible overflow errors when
+    # length(x) exceeds 77_936 (32 bit) or 5_107_605_667 (64 bit)
     (npairs + ndoubleties - ntiesx - ntiesy - 2 * nswaps) /
      sqrt(float(npairs - ntiesx) * float(npairs - ntiesy))
-end
-
-"""
-    countties(x::RealVector, lo::Integer, hi::Integer)
-
-Return the number of ties within `x[lo:hi]`. Assumes `x` is sorted. 
-"""
-function countties(x::AbstractVector, lo::Integer, hi::Integer)
-    # Use widen to avoid overflows on both 32bit and 64bit
-    thistiecount, result = widen(0), widen(0)
-    (lo < 1 || hi > length(x)) && error("Bounds error")
-    @inbounds for i = (lo + 1):hi
-        if x[i] == x[i - 1]
-            thistiecount += 1
-        elseif thistiecount > 0
-            result += div(thistiecount * (thistiecount + 1), 2)
-            thistiecount = 0
-        end
-    end
-
-    if thistiecount > 0
-        result += div(thistiecount * (thistiecount + 1), 2)
-    end
-    result
 end
 
 """
@@ -106,16 +84,23 @@ end
 Compute Kendall's rank correlation coefficient, τ. `x` and `y` must both be either
 matrices or vectors.
 """
-corkendall(x::RealVector, y::RealVector) = corkendall!(float(copy(x)), float(copy(y)))
+corkendall(x::RealVector, y::RealVector) = corkendall!(copy(x), copy(y))
 
-corkendall(X::RealMatrix, y::RealVector) = (permy = sortperm(y);Float64[corkendall!(float(copy(y)), float(X[:,i]), permy) for i in 1:size(X, 2)])
+function corkendall(X::RealMatrix, y::RealVector)
+    permy = sortperm(y)
+    return([corkendall!(copy(y), X[:,i], permy) for i in 1:size(X, 2)])
+end
 
-corkendall(x::RealVector, Y::RealMatrix) = (n = size(Y, 2); permx = sortperm(x); reshape(Float64[corkendall!(float(copy(x)), float(Y[:,i]), permx) for i in 1:n], 1, n))
+function corkendall(x::RealVector, Y::RealMatrix)
+    n = size(Y, 2)
+    permx = sortperm(x)
+    return(reshape([corkendall!(copy(x), Y[:,i], permx) for i in 1:n], 1, n))
+end
 
 function corkendall(X::RealMatrix)
     n = size(X, 2)
-    C = ones(float(eltype(X)), n, n)# Avoids dependency on LinearAlgebra
-    @inbounds for j = 2:n
+    C = Matrix{float(eltype(X))}(I, n, n)
+    for j = 2:n
         permx = sortperm(X[:,j])
         for i = 1:j - 1
             C[j,i] = corkendall!(X[:,j], X[:,i], permx)
@@ -128,8 +113,8 @@ end
 function corkendall(X::RealMatrix, Y::RealMatrix)
     nr = size(X, 2)
     nc = size(Y, 2)
-    C = zeros(float(eltype(X)), nr, nc)
-    @inbounds for j = 1:nr
+    C = Matrix{float(eltype(X))}(undef, nr, nc)
+    for j = 1:nr
         permx = sortperm(X[:,j])
         for i = 1:nc
             C[j,i] = corkendall!(X[:,j], Y[:,i], permx)
@@ -140,29 +125,56 @@ end
 
 # Auxilliary functions for Kendall's rank correlation
 
+"""
+    countties(x::RealVector, lo::Integer, hi::Integer)
+
+Return the number of ties within `x[lo:hi]`. Assumes `x` is sorted. 
+"""
+function countties(x::AbstractVector, lo::Integer, hi::Integer)
+    # Use of widen below prevents possible overflow errors when
+    # length(x) exceeds 2^16 (32 bit) or 2^32 (64 bit)
+    w0 = widen(0)
+    thistiecount, result = w0, w0
+    checkbounds(x, lo:hi)
+    @inbounds for i = (lo + 1):hi
+        if x[i] == x[i - 1]
+            thistiecount += 1
+        elseif thistiecount > 0
+            result += div(thistiecount * (thistiecount + 1), 2)
+            thistiecount = w0
+        end
+    end
+
+    if thistiecount > 0
+        result += div(thistiecount * (thistiecount + 1), 2)
+    end
+    result
+end
+
 # Tests appear to show that a value of 64 is optimal,
 # but note that the equivalent constant in base/sort.jl is 20.
-const SMALL_THRESHOLD  = 64
+const SMALL_THRESHOLD = 64
 
-# Copy was from https://github.com/JuliaLang/julia/commit/28330a2fef4d9d149ba0fd3ffa06347b50067647 dated 20 Sep 2020
+# merge_sort! copied from Julia Base
+# (commit 28330a2fef4d9d149ba0fd3ffa06347b50067647, dated 20 Sep 2020)
 """
-    msort!(v::AbstractVector, lo::Integer, hi::Integer, t=similar(v, 0))    
+    merge_sort!(v::AbstractVector, lo::Integer, hi::Integer, t=similar(v, 0))    
 
 Mutates `v` by sorting elements `x[lo:hi]` using the merge sort algorithm. 
-This method is a copy-paste-edit of sort! in base/sort.jl (the method specialised on MergeSortAlg),
-but amended to return the bubblesort distance.
+This method is a copy-paste-edit of sort! in base/sort.jl, amended to return the bubblesort distance.
 """
-function msort!(v::AbstractVector, lo::Integer, hi::Integer, t=similar(v, 0))
-    # Avoid overflow errors by using widen.
+function merge_sort!(v::AbstractVector, lo::Integer, hi::Integer, t=similar(v, 0))
+    # Use of widen below prevents possible overflow errors when
+    # length(v) exceeds 2^16 (32 bit) or 2^32 (64 bit)
     nswaps = widen(0)
     @inbounds if lo < hi
-        hi - lo <= SMALL_THRESHOLD && return isort!(v, lo, hi)
+        hi - lo <= SMALL_THRESHOLD && return insertion_sort!(v, lo, hi)
 
         m = midpoint(lo, hi)
         (length(t) < m - lo + 1) && resize!(t, m - lo + 1)
 
-        nswaps = msort!(v, lo,  m, t)
-        nswaps += msort!(v, m + 1, hi, t)
+        nswaps = merge_sort!(v, lo,  m, t)
+        nswaps += merge_sort!(v, m + 1, hi, t)
 
         i, j = 1, lo
         while j <= m
@@ -192,27 +204,26 @@ function msort!(v::AbstractVector, lo::Integer, hi::Integer, t=similar(v, 0))
     return nswaps
 end
 
-# This function is also copied from base/sort.jl
+# insertion_sort! and midpoint copied from Julia Base
+# (commit 28330a2fef4d9d149ba0fd3ffa06347b50067647, dated 20 Sep 2020)
 midpoint(lo::T, hi::T) where T <: Integer = lo + ((hi - lo) >>> 0x01)
 midpoint(lo::Integer, hi::Integer) = midpoint(promote(lo, hi)...)
 
-# Copy was from https://github.com/JuliaLang/julia/commit/28330a2fef4d9d149ba0fd3ffa06347b50067647 dated 20 Sep 2020
 """
-    isort!(v::AbstractVector, lo::Integer, hi::Integer)
+    insertion_sort!(v::AbstractVector, lo::Integer, hi::Integer)
 
 Mutates `v` by sorting elements `x[lo:hi]` using the insertion sort algorithm. 
-This method is a copy-paste-edit of sort! in base/sort.jl (the method specialised on InsertionSortAlg),
-amended to return the bubblesort distance.
+This method is a copy-paste-edit of sort! in base/sort.jl, amended to return the bubblesort distance.
 """
-function isort!(v::AbstractVector, lo::Integer, hi::Integer)
-    if lo == hi return 0.0 end
-    nswaps = 0.0
+function insertion_sort!(v::AbstractVector, lo::Integer, hi::Integer)
+    if lo == hi return widen(0) end
+    nswaps = widen(0)
     @inbounds for i = lo + 1:hi
         j = i
         x = v[i]
         while j > lo
             if x < v[j - 1]
-                nswaps += 1.0
+                nswaps += 1
                 v[j] = v[j - 1]
                 j -= 1
                 continue
