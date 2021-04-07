1cde32b5dab06f8209de1120bfb7cbb23542e5e7
Steven G. Johnson <stevenj@mit.edu>
2021-04-07 08:41:36 -0400

    Update src/sampling.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/sampling.jl
+++ src/sampling.jl
@@ -493,8 +493,8 @@ Select a random, optionally weighted sample of size `n` from an array `a`
 using a polyalgorithm. Sampling probabilities are proportional to the weights
 given in `wv`, if provided. `replace` dictates whether sampling is performed
 with replacement. `ordered` dictates whether
-an ordered sample (also called a sequential sample)—a sample where
-items appear in the same order as in `a`—should be taken.
+an ordered sample (also called a sequential sample, i.e. a sample where
+items appear in the same order as in `a`) should be taken.
 
 Optionally specify a random number generator `rng` as the first argument
 (defaults to `Random.GLOBAL_RNG`).
