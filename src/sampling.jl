effb288e72201c37def15ab6bd6f5132906cc3ec
Steven G. Johnson <stevenj@mit.edu>
2021-04-07 08:42:14 -0400

    Update src/sampling.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/sampling.jl
+++ src/sampling.jl
@@ -980,8 +980,8 @@ Select a weighted random sample of size `n` from `a` with probabilities proporti
 to the weights given in `w` if `a` is present, otherwise select a random sample of size
 `n` of the weights given in `w`. `replace` dictates whether sampling is performed with
 replacement. `ordered` dictates whether
-an ordered sample (also called a sequential sample)—a sample where
-items appear in the same order as in `a`—should be taken.
+an ordered sample (also called a sequential sample, i.e. a sample where
+items appear in the same order as in `a`) should be taken.
 
 Optionally specify a random number generator `rng` as the first argument
 (defaults to `Random.GLOBAL_RNG`).
