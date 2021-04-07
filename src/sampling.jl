0c48530f1dfc5a7dffefd0e3d655664659ea4458
Steven G. Johnson <stevenj@mit.edu>
2021-04-07 08:42:04 -0400

    Update src/sampling.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/sampling.jl
+++ src/sampling.jl
@@ -944,9 +944,9 @@ sample(a::AbstractArray, wv::AbstractWeights, dims::Dims;
 
 Select a weighted sample from an array `a` and store the result in `x`. Sampling
 probabilities are proportional to the weights given in `w`. `replace` dictates
-whether sampling is performed with replacement. . `ordered` dictates whether
-an ordered sample (also called a sequential sample)—a sample where
-items appear in the same order as in `a`—should be taken.
+whether sampling is performed with replacement. `ordered` dictates whether
+an ordered sample (also called a sequential sample, i.e. a sample where
+items appear in the same order as in `a`) should be taken.
 
 Optionally specify a random number generator `rng` as the first argument
 (defaults to `Random.GLOBAL_RNG`).
