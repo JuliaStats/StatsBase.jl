56f6e1318900239ae038b03e424fee9c048c1dec
Steven G. Johnson <stevenj@mit.edu>
2021-04-07 08:41:50 -0400

    Update src/sampling.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/sampling.jl
+++ src/sampling.jl
@@ -514,8 +514,8 @@ Select a random, optionally weighted sample from an array `a` specifying
 the dimensions `dims` of the output array. Sampling probabilities are
 proportional to the weights given in `wv`, if provided. `replace` dictates
 whether sampling is performed with replacement. `ordered` dictates whether
-an ordered sample (also called a sequential sample)—a sample where
-items appear in the same order as in `a`—should be taken.
+an ordered sample (also called a sequential sample, i.e. a sample where
+items appear in the same order as in `a`) should be taken.
 
 Optionally specify a random number generator `rng` as the first argument
 (defaults to `Random.GLOBAL_RNG`).
