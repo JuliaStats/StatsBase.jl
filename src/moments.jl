1018a899b56bf437d2bc8cda10cce2d8af8fda13
Yuval <yharel@campus.technion.ac.il>
2021-05-23 17:31:48 +0300

    `mean_and_var` doc fix

--- src/moments.jl
+++ src/moments.jl
@@ -153,7 +153,7 @@ std(v::RealArray, w::AbstractWeights, dim::Int; mean=nothing,
 """
     mean_and_var(x, [w::AbstractWeights], [dim]; corrected=false) -> (mean, var)
 
-Return the mean and standard deviation of collection `x`. If `x` is an `AbstractArray`,
+Return the mean and variance of collection `x`. If `x` is an `AbstractArray`,
 `dim` can be specified as a tuple to compute statistics over these dimensions.
 A weighting vector `w` can be specified to weight the estimates.
 Finally, bias correction is be applied to the variance calculation if `corrected=true`.
