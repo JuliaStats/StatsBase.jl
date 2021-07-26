ff8da3e64d90b73c739bcb820be018a5d053a0b3
Jose Storopoli <thestoropoli@gmail.com>
2021-07-26 10:33:52 -0300

    corrected tests

--- test/reliability.jl
+++ test/reliability.jl
@@ -6,10 +6,10 @@ using LinearAlgebra, Random, Test
              6 11 6 6;
              6 6 12 6;
              6 6 6 13]
-    reliabity_x = crombach_alpha(cov_X)
+    reliability_x = crombach_alpha(cov_X)
     @test reliability_x.alpha ≈ 0.8135593220338981
-    @test reliabity_x.dropped[1].second ≈ 0.75
-    @test reliabity_x.dropped[2].second ≈ 0.7605633802816901
-    @test reliabity_x.dropped[3].second ≈ 0.7714285714285715
-    @test reliabity_x.dropped[4].second ≈ 0.782608695652174
+    @test reliability_x.dropped[1].second ≈ 0.75
+    @test reliability_x.dropped[2].second ≈ 0.7605633802816901
+    @test reliability_x.dropped[3].second ≈ 0.7714285714285715
+    @test reliability_x.dropped[4].second ≈ 0.782608695652174
 end # @testset "StatsBase.Reliability"
