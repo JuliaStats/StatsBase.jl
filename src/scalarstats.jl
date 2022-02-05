ab29aa706e39407ceca7bbe8ac95be71d6ea9de7
Carlos Parada <paradac@carleton.edu>
2022-02-05 07:47:15 -0800

    Update src/scalarstats.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/scalarstats.jl
+++ src/scalarstats.jl
@@ -286,7 +286,7 @@ function sem(x; mean=nothing)
     if isempty(x)
         # Return the NaN of the type that we would get for a nonempty x
         T = eltype(x)
-        _mean = (mean === nothing) ? zero(T) / 1 : mean
+        _mean = mean === nothing ? zero(T) / 1 : mean
         z = abs2(zero(T) - _mean)
         return oftype((z + z) / 2, NaN)
     elseif mean === nothing
