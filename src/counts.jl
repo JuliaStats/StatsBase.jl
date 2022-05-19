fbdf5642b61ef2d6ddd744266a4b106a61ce6196
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2022-05-19 12:54:19 -0500

    Update src/counts.jl

--- src/counts.jl
+++ src/counts.jl
@@ -19,7 +19,7 @@ end
     addcounts!(r, x, levels::UnitRange{<:Integer}, [wv::AbstractWeights])
 
 Add the number of occurrences in `x` of each value in `levels` to an existing
-array `r`.
+array `r`. For each `xi ∈ x`, if `xi == levels[j]`, then we increment `r[j]`.
 
 If a weighting vector `wv` is specified, the sum of weights is used rather than the
 raw counts.
