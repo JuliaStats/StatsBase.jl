3520205fc7f108dbd83a635a2cc1b3a9001c87bc
David Widmann <david.widmann@it.uu.se>
2023-06-20 13:10:43 +0200

    Fix Aqua tests on Julia 1.0

--- test/aqua.jl
+++ test/aqua.jl
@@ -5,7 +5,12 @@ using Test
 @testset "Aqua" begin
     Aqua.test_all(
         StatsBase;
-        ambiguities=(exclude = [sort!],), # ambiguity in SortingAlgorithms (ignore)
-        piracy=(broken = true,), # should ignore `describe`, `pairwise`, `pairwise!` etc. which are owned by StatsBase once supported by Aqua
+        # Ambiguity checks error due to a bug in Aqua on older Julia versions:
+        # https://github.com/JuliaTesting/Aqua.jl/issues/141
+        # We also ignore an ambiguity introduced by SortingAlgorithms
+        ambiguities=VERSION >= v"1.6" ? (exclude = [sort!],) : false,
+        # We should ignore `describe`, `pairwise`, `pairwise!` etc. which are owned by StatsBase once supported by Aqua:
+        # https://github.com/JuliaTesting/Aqua.jl/pull/140
+        piracy=(broken = true,),
     ) 
 end
