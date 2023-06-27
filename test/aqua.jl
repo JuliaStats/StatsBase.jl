34ded45be72621e7fbb5eb7ec664601bb58c848b
David Widmann <david.widmann@it.uu.se>
2023-06-27 14:47:30 +0200

    Fix piracy tests

--- test/aqua.jl
+++ test/aqua.jl
@@ -5,12 +5,12 @@ using Test
 @testset "Aqua" begin
     Aqua.test_all(
         StatsBase;
-        # Ambiguity checks error due to a bug in Aqua on older Julia versions:
-        # https://github.com/JuliaTesting/Aqua.jl/issues/141
-        # We also ignore an ambiguity introduced by SortingAlgorithms
-        ambiguities=VERSION >= v"1.6" ? (exclude = [sort!],) : false,
-        # We should ignore `describe`, `pairwise`, `pairwise!` etc. which are owned by StatsBase once supported by Aqua:
-        # https://github.com/JuliaTesting/Aqua.jl/pull/140
-        piracy=(broken = true,),
+        # Ignore an ambiguity introduced by SortingAlgorithms
+        # Disable test on Julia < 1.6 due to ambiguities in ChainRulesCore
+        # (fixed in more recent versions not supported by Julia < 1.6)
+        ambiguities=VERSION < v"1.6" ? false : (exclude = [sort!],),
+        # `describe`, `pairwise`, `pairwise!`, and `weights` are defined in StatsAPI but owned by StasBase
+        # `quantile(::AbstractVector{<:Real})` is a deprecated method that should be removed
+        piracy=(treat_as_own = [describe, pairwise, pairwise!, weights, quantile],),
     ) 
 end
