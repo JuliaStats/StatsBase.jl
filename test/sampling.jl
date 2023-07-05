e85e008b017490382a5ad0b0c7fbe90d53e2a6dc
Alex Arslan <ararslan@comcast.net>
2023-07-05 09:01:56 -0700

    Fix `sample` with wide integer types (#873)
    As observed in issue 872, an unordered sample without replacement with a
    type 64 bits or wider (notably excluding `Int64`, at least on a 64-bit
    system) hits a `MethodError` in `samplepair`, which had been restricted
    to `Int` inputs. Relaxing this restriction and adjusting the
    `samplepair` implementation accordingly allows these cases to succeed.

--- test/sampling.jl
+++ test/sampling.jl
@@ -91,6 +91,10 @@ test_rng_use(sample, 1:10, 10)
 
     @test samplepair(rng, [3, 4, 2, 6, 8]) === (3, 8)
     @test samplepair(rng, [1, 2])          === (1, 2)
+
+    onetwo = samplepair(rng, UInt128(2))
+    @test extrema(onetwo) == (1, 2)
+    @test eltype(onetwo) === UInt128
 end
 
 test_rng_use(samplepair, 1000)
@@ -264,5 +268,28 @@ test_same(replace=false, ordered=false)
         @test_throws ArgumentError f(view(x, 2:4), view(x, 3:5))
         # This corner case should succeed
         f(view(x, 2:4), view(x, 5:6))
+    end
+end
+
+@testset "issue #872" begin
+    for T in [Int8, Int16, Int32, Int64, Int128, BigInt], f in [identity, unsigned]
+        T == BigInt && f == unsigned && continue
+        T = f(T)
+        # The type of the second argument should not affect the return type
+        let samp = sample(T(1):T(10), T(2); replace=false, ordered=false)
+            @test all(x -> x isa T, samp)
+            @test all(x -> T(1) <= x <= T(10), samp)
+            @test length(samp) == 2
+        end
+        let samp = sample(T(1):T(10), 2; replace=false, ordered=false)
+            @test all(x -> x isa T, samp)
+            @test all(x -> T(1) <= x <= T(10), samp)
+            @test length(samp) == 2
+        end
+        let samp = sample(1:10, T(2); replace=false, ordered=false)
+            @test all(x -> x isa Int, samp)
+            @test all(x -> 1 <= x <= 10, samp)
+            @test length(samp) == 2
+        end
     end
 end
