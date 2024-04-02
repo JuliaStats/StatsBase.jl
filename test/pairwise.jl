1a4d064d63cd2b09f3153e5524656c07d407d077
Philip Swannell <18028484+PGS62@users.noreply.github.com>
2024-04-02 16:13:16 +0100

    Deleted commented out tests

--- test/pairwise.jl
+++ test/pairwise.jl
@@ -53,9 +53,7 @@ arbitrary_fun(x, y) = cor(x, y)
 
         @inferred pairwise(f, x, y)
         if throwsforzerolengthinput
-            # @test_throws Union{ArgumentError,MethodError} pairwise(f, [Int[]], [Int[]])
             @test_throws CompositeException pairwise(f, [Int[]], [Int[]])
-            #@test_throws Union{ArgumentError,MethodError} pairwise!(f, zeros(1, 1), [Int[]], [Int[]])
             @test_throws CompositeException pairwise!(f, zeros(1, 1), [Int[]], [Int[]])
         end
 
@@ -122,23 +120,18 @@ arbitrary_fun(x, y) = cor(x, y)
             return x[which], y[which]
         end
 
-        #@test isapprox(res, [f(collect.(skipmissings(xi, yi))...) for xi in xm, yi in ym],
-        #    rtol=1e-6)
         @test isapprox(res, [f(myskipmissings(xi, yi)...) for xi in xm, yi in ym],
             rtol=1e-6)
 
         res = pairwise(f, ym, zm, skipmissing=:pairwise)
         if isrankcor
             @test res isa Matrix{Float64}
-            #    @test isequal(res, [f(collect.(skipmissings(yi, zi))...) for yi in ym, zi in zm])
             @test isequal(res, [f(myskipmissings(yi, zi)...) for yi in ym, zi in zm])
         else
             @test res isa Matrix{Float32}
             res2 = zeros(Union{Float32,Missing}, size(res))
             @test pairwise!(f, res2, ym, zm, skipmissing=:pairwise) === res2
             @test res ≅ res2
-            #@test isapprox(res, [f(collect.(skipmissings(yi, zi))...) for yi in ym, zi in zm],
-            #    rtol=1e-6)
             @test isapprox(res, [f(myskipmissings(yi, zi)...) for yi in ym, zi in zm],
                 rtol=1e-6)
 
@@ -189,15 +182,11 @@ arbitrary_fun(x, y) = cor(x, y)
 
         if VERSION >= v"1.5" # Fails with UndefVarError on Julia 1.0
             if throwsforzerolengthinput
-                #@test_throws Union{ArgumentError,MethodError} pairwise(f, xm, ym, skipmissing=:pairwise)
                 @test_throws CompositeException pairwise(f, xm, ym, skipmissing=:pairwise)
-                #@test_throws Union{ArgumentError,MethodError} pairwise(f, xm, ym, skipmissing=:listwise)
                 @test_throws CompositeException pairwise(f, xm, ym, skipmissing=:listwise)
 
                 res = zeros(Union{Float64,Missing}, length(xm), length(ym))
-                #@test_throws Union{ArgumentError,MethodError} pairwise!(f, res, xm, ym, skipmissing=:pairwise)
                 @test_throws CompositeException pairwise!(f, res, xm, ym, skipmissing=:pairwise)
-                #@test_throws Union{ArgumentError,MethodError} pairwise!(f, res, xm, ym, skipmissing=:listwise)
                 @test_throws CompositeException pairwise!(f, res, xm, ym, skipmissing=:listwise)
             end
         end
