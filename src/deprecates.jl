dfe8945a90638de0a42d8bc7a8eb6f4328d2a358
David Widmann <devmotion@users.noreply.github.com>
2025-05-04 21:37:37 +0200

    Add JET tests (#955)

--- src/deprecates.jl
+++ src/deprecates.jl
@@ -5,7 +5,7 @@ if !isdefined(Base, :stderr)
 else
     function (io::typeof(stderr))(obj::StatisticalModel)
         Base.depwarn("stderr(obj::StatisticalModel) is deprecated, use stderror(obj) instead", :stderr)
-        io === stderr ? stderror(obj) : throw(MethodErrror(io, (obj,)))
+        io === stderr ? stderror(obj) : throw(MethodError(io, (obj,)))
     end
 end
 
@@ -22,8 +22,6 @@ end
 @deprecate scattermatm(x::DenseMatrix, mean, wv::AbstractWeights, dims::Int) scattermat(x, wv, mean=mean, dims=dims)
 @deprecate scattermat(x::DenseMatrix, dims::Int) scattermat(x, dims=dims)
 @deprecate scattermat(x::DenseMatrix, wv::AbstractWeights, dims::Int) scattermat(x, wv, dims=dims)
-@deprecate scattermat_zm(x::DenseMatrix, dims::Int) scattermat_zm(x, dims=dims)
-@deprecate scattermat_zm(x::DenseMatrix, wv::AbstractWeights, dims::Int) scattermat_zm(x::DenseMatrix, wv::AbstractWeights, dims=dims)
 @deprecate mean!(R::AbstractArray, A::AbstractArray, w::AbstractWeights, dims::Int) mean!(R, A, w, dims=dims)
 @deprecate mean(A::AbstractArray{T}, w::AbstractWeights{W}, dims::Int) where {T<:Number,W<:Real} mean(A, w, dims=dims)
 
