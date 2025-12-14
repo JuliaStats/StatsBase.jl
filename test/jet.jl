0ef9627928d7c0639893f60777ba35f32b7bea2e
Milan Bouchet-Valat <nalimilan@club.fr>
2025-12-14 22:51:16 +0100

    Fix JET test

--- test/jet.jl
+++ test/jet.jl
@@ -11,12 +11,13 @@ if VERSION < v"1.12-"
         # Default error analysis for common problem fails since JET errors on interface definitions
         # The (deprecated) `model_response(::StatisticalModel)` calls the interface
         # function `response(::StatisticalModel)` for which no method exists yet
+        # JET is also confused by any(ismissing, ::AbstractVector)
         # Note: This analysis is not enough strict to guarantee that there are no runtime errors!
         # Ref https://github.com/aviatesk/JET.jl/issues/495
         res = JET.report_package("StatsBase"; target_defined_modules = true, mode = :basic, toplevel_logger = nothing)
         println(res)
         reports = JET.get_reports(res)
         @test_broken isempty(reports)
-        @test length(reports) <= 1
+        @test length(reports) <= 2
     end
 end
