299bd06eea059f44673f1d9f4e5a679ca3c15973
Milan Bouchet-Valat <nalimilan@club.fr>
2026-01-11 18:05:06 +0100

    Run Runic on code base

--- test/robust.jl
+++ test/robust.jl
@@ -3,61 +3,61 @@ using Test, Random
 
 ### Trimming outliers
 
-@test collect(trim([8,2,3,4,5,6,7,1], prop=0.1)) == [8,2,3,4,5,6,7,1]
-@test collect(trim([8,2,3,4,5,6,7,1], prop=0.2)) == [2,3,4,5,6,7]
-@test collect(trim([1,2,3,4,5,6,7,8,9], prop=0.4)) == [4,5,6]
-@test collect(trim([8,7,6,5,4,3,2,1], count=1)) == [7,6,5,4,3,2]
-@test collect(trim([1,2,3,4,5,6,7,8,9], count=3)) == [4,5,6]
+@test collect(trim([8, 2, 3, 4, 5, 6, 7, 1], prop = 0.1)) == [8, 2, 3, 4, 5, 6, 7, 1]
+@test collect(trim([8, 2, 3, 4, 5, 6, 7, 1], prop = 0.2)) == [2, 3, 4, 5, 6, 7]
+@test collect(trim([1, 2, 3, 4, 5, 6, 7, 8, 9], prop = 0.4)) == [4, 5, 6]
+@test collect(trim([8, 7, 6, 5, 4, 3, 2, 1], count = 1)) == [7, 6, 5, 4, 3, 2]
+@test collect(trim([1, 2, 3, 4, 5, 6, 7, 8, 9], count = 3)) == [4, 5, 6]
 
 
 @test_throws ArgumentError trim([])
-@test_throws ArgumentError trim([1,2,3,4,5], prop=0.5)
+@test_throws ArgumentError trim([1, 2, 3, 4, 5], prop = 0.5)
 
-@test collect(trim!([8,2,3,4,5,6,7,1], prop=0.1)) == [8,2,3,4,5,6,7,1]
-@test collect(trim!([8,2,3,4,5,6,7,1], prop=0.2)) == [2,3,4,5,6,7]
-@test collect(trim!([1,2,3,4,5,6,7,8,9], prop=0.4)) == [4,5,6]
-@test collect(trim!([8,7,6,5,4,3,2,1], count=1)) == [7,6,5,4,3,2]
-@test collect(trim!([1,2,3,4,5,6,7,8,9], count=3)) == [4,5,6]
+@test collect(trim!([8, 2, 3, 4, 5, 6, 7, 1], prop = 0.1)) == [8, 2, 3, 4, 5, 6, 7, 1]
+@test collect(trim!([8, 2, 3, 4, 5, 6, 7, 1], prop = 0.2)) == [2, 3, 4, 5, 6, 7]
+@test collect(trim!([1, 2, 3, 4, 5, 6, 7, 8, 9], prop = 0.4)) == [4, 5, 6]
+@test collect(trim!([8, 7, 6, 5, 4, 3, 2, 1], count = 1)) == [7, 6, 5, 4, 3, 2]
+@test collect(trim!([1, 2, 3, 4, 5, 6, 7, 8, 9], count = 3)) == [4, 5, 6]
 
 @test_throws ArgumentError trim!([])
-@test_throws ArgumentError trim!([1,2,3,4,5], prop=0.5)
+@test_throws ArgumentError trim!([1, 2, 3, 4, 5], prop = 0.5)
 
-@test collect(winsor([8,2,3,4,5,6,7,1], prop=0.1)) == [8,2,3,4,5,6,7,1]
-@test collect(winsor([8,2,3,4,5,6,7,1], prop=0.2)) == [7,2,3,4,5,6,7,2]
-@test collect(winsor([1,2,3,4,5,6,7,8,9], prop=0.4)) == [4,4,4,4,5,6,6,6,6]
-@test collect(winsor([1,2,3,4,5,6,7,8], count=1)) == [2,2,3,4,5,6,7,7]
-@test collect(winsor([8,7,6,5,4,3,2,1], count=1)) == [7,7,6,5,4,3,2,2]
-@test collect(winsor([1,2,3,4,5,6,7,8,9], count=3)) == [4,4,4,4,5,6,6,6,6]
+@test collect(winsor([8, 2, 3, 4, 5, 6, 7, 1], prop = 0.1)) == [8, 2, 3, 4, 5, 6, 7, 1]
+@test collect(winsor([8, 2, 3, 4, 5, 6, 7, 1], prop = 0.2)) == [7, 2, 3, 4, 5, 6, 7, 2]
+@test collect(winsor([1, 2, 3, 4, 5, 6, 7, 8, 9], prop = 0.4)) == [4, 4, 4, 4, 5, 6, 6, 6, 6]
+@test collect(winsor([1, 2, 3, 4, 5, 6, 7, 8], count = 1)) == [2, 2, 3, 4, 5, 6, 7, 7]
+@test collect(winsor([8, 7, 6, 5, 4, 3, 2, 1], count = 1)) == [7, 7, 6, 5, 4, 3, 2, 2]
+@test collect(winsor([1, 2, 3, 4, 5, 6, 7, 8, 9], count = 3)) == [4, 4, 4, 4, 5, 6, 6, 6, 6]
 
 @test_throws ArgumentError winsor([])
-@test_throws ArgumentError winsor([1,2,3,4,5], prop=0.5)
+@test_throws ArgumentError winsor([1, 2, 3, 4, 5], prop = 0.5)
 
-@test collect(winsor!([8,2,3,4,5,6,7,1], prop=0.1)) == [8,2,3,4,5,6,7,1]
-@test collect(winsor!([8,2,3,4,5,6,7,1], prop=0.2)) == [7,2,3,4,5,6,7,2]
-@test collect(winsor!([1,2,3,4,5,6,7,8,9], prop=0.4)) == [4,4,4,4,5,6,6,6,6]
-@test collect(winsor!([8,7,6,5,4,3,2,1], count=1)) == [7,7,6,5,4,3,2,2]
-@test collect(winsor!([1,2,3,4,5,6,7,8,9], count=3)) == [4,4,4,4,5,6,6,6,6]
+@test collect(winsor!([8, 2, 3, 4, 5, 6, 7, 1], prop = 0.1)) == [8, 2, 3, 4, 5, 6, 7, 1]
+@test collect(winsor!([8, 2, 3, 4, 5, 6, 7, 1], prop = 0.2)) == [7, 2, 3, 4, 5, 6, 7, 2]
+@test collect(winsor!([1, 2, 3, 4, 5, 6, 7, 8, 9], prop = 0.4)) == [4, 4, 4, 4, 5, 6, 6, 6, 6]
+@test collect(winsor!([8, 7, 6, 5, 4, 3, 2, 1], count = 1)) == [7, 7, 6, 5, 4, 3, 2, 2]
+@test collect(winsor!([1, 2, 3, 4, 5, 6, 7, 8, 9], count = 3)) == [4, 4, 4, 4, 5, 6, 6, 6, 6]
 
 @test_throws ArgumentError winsor!([])
-@test_throws ArgumentError winsor!([1,2,3,4,5], prop=0.5)
+@test_throws ArgumentError winsor!([1, 2, 3, 4, 5], prop = 0.5)
 
 ### Variance
 
-@test trimvar([1,1,1,1,1]) ≈ 0.0
-@test trimvar([2,3,4,5,6,7,8,9], prop=0.25) ≈ 1.0
+@test trimvar([1, 1, 1, 1, 1]) ≈ 0.0
+@test trimvar([2, 3, 4, 5, 6, 7, 8, 9], prop = 0.25) ≈ 1.0
 
 @test_throws ArgumentError trimvar([])
-@test_throws ArgumentError trimvar([1,2,3,4,5], prop=0.5)
+@test_throws ArgumentError trimvar([1, 2, 3, 4, 5], prop = 0.5)
 
 ### Other
 
-@test mean(trim([-Inf,1,2,3,4], count=1)) == 2
-@test mean(winsor([-Inf,1,2,3,4], count=1)) == 2
+@test mean(trim([-Inf, 1, 2, 3, 4], count = 1)) == 2
+@test mean(winsor([-Inf, 1, 2, 3, 4], count = 1)) == 2
 
 Random.seed!(1234)
 for n in 2100:2120, c in 0:1000
     x = randperm(n)
-    @test sort!(collect(winsor(x, count=c))) ==
-          reverse!(collect(winsor(n:-1:1, count=c))) ==
-          collect(winsor(1:n, count=c))
+    @test sort!(collect(winsor(x, count = c))) ==
+        reverse!(collect(winsor(n:-1:1, count = c))) ==
+        collect(winsor(1:n, count = c))
 end
--- test/wsampling.jl
+++ test/wsampling.jl
@@ -5,28 +5,30 @@ Random.seed!(1234)
 
 #### weighted sample with replacement
 
-function check_wsample_wrep(a::AbstractArray, vrgn, wv::AbstractWeights, ptol::Real;
-                            ordered::Bool=false, rev::Bool=false)
+function check_wsample_wrep(
+        a::AbstractArray, vrgn, wv::AbstractWeights, ptol::Real;
+        ordered::Bool = false, rev::Bool = false
+    )
     K = length(wv)
     (vmin, vmax) = vrgn
     (amin, amax) = extrema(a)
     @test vmin <= amin <= amax <= vmax
     p0 = wv ./ sum(wv)
     rev && reverse!(p0)
-    if ordered
-        @test issorted(a; rev=rev)
+    return if ordered
+        @test issorted(a; rev = rev)
         if ptol > 0
-            @test isapprox(proportions(a, vmin:vmax), p0, atol=ptol)
+            @test isapprox(proportions(a, vmin:vmax), p0, atol = ptol)
         end
     else
-        @test !issorted(a; rev=rev)
-        ncols = size(a,2)
+        @test !issorted(a; rev = rev)
+        ncols = size(a, 2)
         if ncols == 1
-            @test isapprox(proportions(a, vmin:vmax), p0, atol=ptol)
+            @test isapprox(proportions(a, vmin:vmax), p0, atol = ptol)
         else
-            for j = 1:ncols
+            for j in 1:ncols
                 aj = view(a, :, j)
-                @test isapprox(proportions(aj, vmin:vmax), p0, atol=ptol)
+                @test isapprox(proportions(aj, vmin:vmax), p0, atol = ptol)
             end
         end
     end
@@ -38,21 +40,21 @@ n = 10^6
 wv = weights([0.2, 0.8, 0.4, 0.6])
 
 for wv in (
-    weights([0.2, 0.8, 0.4, 0.6]),
-    weights([2, 8, 4, 6]),
-    weights(Float32[0.2, 0.8, 0.4, 0.6]),
-    Weights(Float32[0.2, 0.8, 0.4, 0.6], 2),
-    Weights([2, 8, 4, 6], 20.0),
-)
+        weights([0.2, 0.8, 0.4, 0.6]),
+        weights([2, 8, 4, 6]),
+        weights(Float32[0.2, 0.8, 0.4, 0.6]),
+        Weights(Float32[0.2, 0.8, 0.4, 0.6], 2),
+        Weights([2, 8, 4, 6], 20.0),
+    )
     a = direct_sample!(4:7, wv, zeros(Int, n, 3))
-    check_wsample_wrep(a, (4, 7), wv, 5.0e-3; ordered=false)
+    check_wsample_wrep(a, (4, 7), wv, 5.0e-3; ordered = false)
     test_rng_use(direct_sample!, 4:7, wv, zeros(Int, 100))
 
     a = alias_sample!(4:7, wv, zeros(Int, n, 3))
-    check_wsample_wrep(a, (4, 7), wv, 5.0e-3; ordered=false)
+    check_wsample_wrep(a, (4, 7), wv, 5.0e-3; ordered = false)
 
-    a = sample(4:7, wv, n; ordered=false)
-    check_wsample_wrep(a, (4, 7), wv, 5.0e-3; ordered=false)
+    a = sample(4:7, wv, n; ordered = false)
+    check_wsample_wrep(a, (4, 7), wv, 5.0e-3; ordered = false)
 end
 
 @test_throws ArgumentError alias_sample!(rand(10), weights(fill(0, 10)), rand(10))
@@ -60,17 +62,19 @@ end
 
 for rev in (true, false), T in (Int, Int16, Float64, Float16, BigInt, ComplexF64, Rational{Int})
     r = rev ? reverse(4:7) : (4:7)
-    r = T===Int ? r : T.(r)
-    aa = Int.(sample(r, wv, n; ordered=true))
-    check_wsample_wrep(aa, (4, 7), wv, 5.0e-3; ordered=true, rev=rev)
-    aa = Int.(sample(r, wv, 10; ordered=true))
-    check_wsample_wrep(aa, (4, 7), wv, -1; ordered=true, rev=rev)
+    r = T === Int ? r : T.(r)
+    aa = Int.(sample(r, wv, n; ordered = true))
+    check_wsample_wrep(aa, (4, 7), wv, 5.0e-3; ordered = true, rev = rev)
+    aa = Int.(sample(r, wv, 10; ordered = true))
+    check_wsample_wrep(aa, (4, 7), wv, -1; ordered = true, rev = rev)
 end
 
 #### weighted sampling without replacement
 
-function check_wsample_norep(a::AbstractArray, vrgn, wv::AbstractWeights, ptol::Real;
-                             ordered::Bool=false, rev::Bool=false)
+function check_wsample_norep(
+        a::AbstractArray, vrgn, wv::AbstractWeights, ptol::Real;
+        ordered::Bool = false, rev::Bool = false
+    )
     # each column of a for one run
 
     vmin, vmax = vrgn
@@ -78,63 +82,63 @@ function check_wsample_norep(a::AbstractArray, vrgn, wv::AbstractWeights, ptol::
     @test vmin <= amin <= amax <= vmax
     n = vmax - vmin + 1
 
-    for j = 1:size(a,2)
-        aj = view(a,:,j)
+    for j in 1:size(a, 2)
+        aj = view(a, :, j)
         @assert allunique(aj)
         if ordered
-            @assert issorted(aj; rev=rev)
+            @assert issorted(aj; rev = rev)
         end
     end
 
-    if ptol > 0
+    return if ptol > 0
         p0 = wv ./ sum(wv)
         rev && reverse!(p0)
-        @test isapprox(proportions(a[1,:], vmin:vmax), p0, atol=ptol)
+        @test isapprox(proportions(a[1, :], vmin:vmax), p0, atol = ptol)
     end
 end
 
 import StatsBase: naive_wsample_norep!, efraimidis_a_wsample_norep!,
-                  efraimidis_ares_wsample_norep!, efraimidis_aexpj_wsample_norep!
+    efraimidis_ares_wsample_norep!, efraimidis_aexpj_wsample_norep!
 
 n = 10^5
 wv = weights([0.2, 0.8, 0.4, 0.6])
 
 a = zeros(Int, 3, n)
-for j = 1:n
-    naive_wsample_norep!(4:7, wv, view(a,:,j))
+for j in 1:n
+    naive_wsample_norep!(4:7, wv, view(a, :, j))
 end
-check_wsample_norep(a, (4, 7), wv, 5.0e-3; ordered=false)
+check_wsample_norep(a, (4, 7), wv, 5.0e-3; ordered = false)
 test_rng_use(naive_wsample_norep!, 4:7, wv, zeros(Int, 2))
 
 a = zeros(Int, 3, n)
-for j = 1:n
-    efraimidis_a_wsample_norep!(4:7, wv, view(a,:,j))
+for j in 1:n
+    efraimidis_a_wsample_norep!(4:7, wv, view(a, :, j))
 end
-check_wsample_norep(a, (4, 7), wv, 5.0e-3; ordered=false)
+check_wsample_norep(a, (4, 7), wv, 5.0e-3; ordered = false)
 test_rng_use(efraimidis_a_wsample_norep!, 4:7, wv, zeros(Int, 2))
 
 a = zeros(Int, 3, n)
-for j = 1:n
-    efraimidis_ares_wsample_norep!(4:7, wv, view(a,:,j))
+for j in 1:n
+    efraimidis_ares_wsample_norep!(4:7, wv, view(a, :, j))
 end
-check_wsample_norep(a, (4, 7), wv, 5.0e-3; ordered=false)
+check_wsample_norep(a, (4, 7), wv, 5.0e-3; ordered = false)
 test_rng_use(efraimidis_ares_wsample_norep!, 4:7, wv, zeros(Int, 2))
 
 a = zeros(Int, 3, n)
-for j = 1:n
-    efraimidis_aexpj_wsample_norep!(4:7, wv, view(a,:,j))
+for j in 1:n
+    efraimidis_aexpj_wsample_norep!(4:7, wv, view(a, :, j))
 end
-check_wsample_norep(a, (4, 7), wv, 5.0e-3; ordered=false)
+check_wsample_norep(a, (4, 7), wv, 5.0e-3; ordered = false)
 test_rng_use(efraimidis_aexpj_wsample_norep!, 4:7, wv, zeros(Int, 2))
 
-a = sample(4:7, wv, 3; replace=false, ordered=false)
-check_wsample_norep(a, (4, 7), wv, -1; ordered=false)
+a = sample(4:7, wv, 3; replace = false, ordered = false)
+check_wsample_norep(a, (4, 7), wv, -1; ordered = false)
 
 for rev in (true, false), T in (Int, Int16, Float64, Float16, BigInt, ComplexF64, Rational{Int})
     r = rev ? reverse(4:7) : (4:7)
-    r = T===Int ? r : T.(r)
-    aa = Int.(sample(r, wv, 3; replace=false, ordered=true))
-    check_wsample_norep(aa, (4, 7), wv, -1; ordered=true, rev=rev)
+    r = T === Int ? r : T.(r)
+    aa = Int.(sample(r, wv, 3; replace = false, ordered = true))
+    check_wsample_norep(aa, (4, 7), wv, -1; ordered = true, rev = rev)
 end
 
 @testset "validation of inputs" begin
@@ -147,8 +151,10 @@ end
 
     @test_throws ArgumentError sample(weights(ox))
 
-    for f in (sample!, wsample!, naive_wsample_norep!, efraimidis_a_wsample_norep!,
-            efraimidis_ares_wsample_norep!, efraimidis_aexpj_wsample_norep!)
+    for f in (
+            sample!, wsample!, naive_wsample_norep!, efraimidis_a_wsample_norep!,
+            efraimidis_ares_wsample_norep!, efraimidis_aexpj_wsample_norep!,
+        )
         # Test that offset arrays throw an error
         @test_throws ArgumentError f(ox, weights(y), z)
         @test_throws ArgumentError f(x, weights(oy), z)
